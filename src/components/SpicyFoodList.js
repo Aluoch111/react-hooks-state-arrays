63dfa79cae7bf4cde9d31e9c4e7ab57cc7159dcc
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-09-30 17:22:31 +0300

    completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,24 +3,70 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filter, setFilter] = useState("All")
 
+  // handle add food
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods , newFood];
+    setFoods(newFoodArray);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+  //  click function
+  function handleClick(id){
+    const newFoodArray = foods.map( (foods) => {
+      if(foods.id === id){
+        return{...foods,heatLevel : foods.heatLevel + 1,};
+      }
+      else{
+        return foods ;
+      }
+    });
+  
+    setFoods(newFoodArray);
+  };
+  
+  // filter 
+
+  const foodsToDisplay = foods.filter((food) => {
+		if (filter === "All") {
+      return true ;
+    } else{
+     return food.cuisine === filter;
+    }
+		
+	});
+
+
+	function handleFilterChange(event) {
+		setFilter( event.target.value);
+	};
+
+
+	const foodList = foodsToDisplay.map((food) => (
+		<li key={food.id} onClick={() => handleClick(food.id)}>
+			{food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+		</li>
+	));
 
   return (
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
+        <select name="filter" onChange={handleFilterChange}>
+            <option value="All">All</option>
+            <option value="American">American</option>
+            <option value="Sichuan">Sichuan</option>
+            <option value="Thai">Thai</option>
+            <option value="Mexican">Mexican</option>
+        </select>
+      
+          <button onClick={handleAddFood}>Add New Food</button>
+          <ul>{foodList}</ul>
     </div>
   );
-}
+  }
+
+
+
+
 
 export default SpicyFoodList;
